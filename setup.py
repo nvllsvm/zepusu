4dfe7e8f8b051442f3e5fbe146e4754f348043d4
Andrew Rabert <ar@nullsum.net>
2019-03-09 14:28:25 -0500

    Remove setuptools_scm It breaks building from a tag's archives. There are work arounds like setuptools_scm_git_archive, but there's way too much configuration involved for me to give a shit.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ REPO = pathlib.Path(__file__).parent
 
 setuptools.setup(
     name='zepusu',
+    version='0.2.0',
     author='Andrew Rabert',
     author_email='ar@nullsum.net',
     url='https://gitlab.com/nvllsvm/zepusu',
@@ -14,8 +15,6 @@ setuptools.setup(
     long_description=REPO.joinpath('README.rst').read_text(),
     py_modules=['zepusu'],
     install_requires=['pyzmq'],
-    setup_requires=['setuptools_scm'],
-    use_scm_version=True,
     entry_points={'console_scripts': ['zepusu=zepusu:main']},
     classifiers=[
         'Development Status :: 3 - Alpha',
